19a310943a42ac6fff6830357a216f04e7faae3d
Nick <hi@nkls.cc>
2015-10-12 19:48:35 +0200

    ↑↑ ↓↓ ←→ ←→ b a

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # js-konami-code
-The glorious Konami Code written in plain javascript for usage on websites.
+### The glorious Konami Code written in plain javascript for usage on websites.
+
+Nothing much to explain here. :)
+
+Will be used soonish™ on niklasmeyer.de
--- konami-code.js
+++ konami-code.js
@@ -6,8 +6,8 @@ function konamiCode(e) {
      * ↑↑ ↓↓ ←→ ←→ b a
      ---*/
     var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var keyCode 	= e.keyCode;
 
-    var keyCode = e.keyCode;
     if(keyCode === konami_keys[konami_index++]) {
         if (konami_index === konami_keys.length) {
             //CODE TO BE EXECTUED AFTER CORRECT CODE IN HERE
@@ -15,4 +15,5 @@ function konamiCode(e) {
     } else {
         konami_index = 0;
     }
+
 }
