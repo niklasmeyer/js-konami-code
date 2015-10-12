8d2950beb1886d04503abd3719ba23ed7a49b99b
Nick <hi@nkls.cc>
2015-10-12 19:43:51 +0200

    ↑↑ ↓↓ ←→ ←→ b a

---
+++ konami-code.js
@@ -0,0 +1,18 @@
+document.addEventListener("keydown", konamiCode, false);
+var konami_index = 0;
+
+function konamiCode(e) {
+    /*---
+     * ↑↑ ↓↓ ←→ ←→ b a
+     ---*/
+    var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+    var keyCode = e.keyCode;
+    if(keyCode === konami_keys[konami_index++]) {
+        if (konami_index === konami_keys.length) {
+            //CODE TO BE EXECTUED AFTER CORRECT CODE IN HERE
+        }
+    } else {
+        konami_index = 0;
+    }
+}
